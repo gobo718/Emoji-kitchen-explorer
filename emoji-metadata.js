d3513cc8bb3f86b6a1fb0c95abb8f2fe9ec8b4e2
gobo718 <gobo718@gmail.com>
2026-07-16 23:21:31 -0400

    Add files via upload
    Basic Emoji Tools

---
+++ emoji-metadata.js
@@ -0,0 +1,11 @@
+window.EMOJI_PROFILE_METADATA = {
+  "🩵": {
+    name: "Light Blue Heart",
+    codepoint: "U+1FA75",
+    unicodeVersion: "15.0",
+    emojiVersion: "15.0",
+    approvedDate: "2022-09-13",
+    historyNote: "Added in Unicode 15.0 and Emoji 15.0.",
+    sourceLabel: "Unicode 15.0"
+  }
+};
